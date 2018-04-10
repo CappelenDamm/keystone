7c473b4f2b0d4b010f069dd9e81636db023b3879
siverv <8384586+siverv@users.noreply.github.com>
2018-04-10 15:36:59 +0200

    JsonType get-value fix

--- fields/types/json/JsonType.js
+++ fields/types/json/JsonType.js
@@ -24,43 +24,22 @@ util.inherits(json, FieldType);
 /* Inherit from TextType prototype */
 json.prototype.addFilterToQuery = TextType.prototype.addFilterToQuery;
 
-
-json.prototype.getValueFromData = function(data, canThrow) {
-	var value = this.path in data ? data[this.path] : this._path.get(data);
-
-	if(typeof value == 'string') {
-		try {
-			value = JSON.parse(value);
-		} catch(ex) {
-			if(canThrow === true) {
-				throw ex;
-			} else {
-				value = null;
-			}
-		}
-	}
-
-	return value;
-};
-
 json.prototype.validateRequiredInput = function (item, data, callback) {
 	var value = this.getValueFromData(data);
-	var result = !!value;
-	if (value === undefined && item.get(this.path)) {
+	var result = value != null;
+	if (value === undefined && item.get(this.path) != null) {
 		result = true;
 	}
 	utils.defer(callback, result);
 };
 
 json.prototype.validateInput = function (data, callback)  {
+    let result = false;
 	try {
-		let value = this.getValueFromData(data, true);
-
-		if(typeof value != 'object') {
-			result = false;
-		} else {
-			result = true;
-		}
+		let value = this.getValueFromData(data);
+        JSON.parse(JSON.stringify(value));
+        result = true
+        // result = JSON.stringify(value) == JSON.stringify(JSON.parse(JSON.stringify(value)));
 	} catch(ex) {
 		result = false;
 	}
